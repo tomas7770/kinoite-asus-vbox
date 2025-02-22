c7e65d28863fa5042e7bb7ec209647fc0c62a7d6
tomas7770 <77364520+tomas7770@users.noreply.github.com>
2025-02-22 12:09:00 +0000

    Switch upstream to kinoite-nvidia
    Install Asus packages in this image and discontinue use of Asus kernel

--- build.sh
+++ build.sh
@@ -2,23 +2,26 @@
 
 set -ouex pipefail
 
-RELEASE="$(rpm -E %fedora)"
-
-
 ### Install packages
 
 # Packages can be installed from any enabled yum repo on the image.
 # Install RPMFusion
-rpm-ostree install \
+dnf install -y \
     https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-41.noarch.rpm \
     https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-41.noarch.rpm
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
+# Install VirtualBox
 find /tmp/rpms
-rpm-ostree install /tmp/rpms/kmods/kmod-VirtualBox*.rpm
-rpm-ostree install VirtualBox
+dnf install -y /tmp/rpms/kmods/kmod-VirtualBox*.rpm
+dnf install -y VirtualBox
 
+# Install Tailscale
 curl https://pkgs.tailscale.com/stable/fedora/tailscale.repo -o /etc/yum.repos.d/tailscale.repo
 chmod 644 /etc/yum.repos.d/tailscale.repo
-rpm-ostree install tailscale
+dnf install -y tailscale
+
+# Install Asus stuff
+dnf5 -y copr enable lukenukem/asus-linux
+dnf5 -y install asusctl asusctl-rog-gui supergfxctl
+# Disable COPRs so they don't end up enabled on the final image:
+dnf5 -y copr disable lukenukem/asus-linux
