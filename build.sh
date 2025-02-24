fc812cdf0f0e55d03bba1666ace24ac74d764eea
tomas7770 <77364520+tomas7770@users.noreply.github.com>
2025-02-24 15:04:49 +0000

    Minor style changes

--- build.sh
+++ build.sh
@@ -5,7 +5,14 @@ set -ouex pipefail
 ### Install packages
 
 # Packages can be installed from any enabled yum repo on the image.
-# Install RPMFusion
+
+# Install Asus stuff
+dnf5 -y copr enable lukenukem/asus-linux
+dnf5 -y install asusctl asusctl-rog-gui supergfxctl
+# Disable COPRs so they don't end up enabled on the final image:
+dnf5 -y copr disable lukenukem/asus-linux
+
+# Install RPMFusion (needed by VirtualBox)
 dnf install -y \
     https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-41.noarch.rpm \
     https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-41.noarch.rpm
@@ -19,9 +26,3 @@ dnf install -y VirtualBox
 curl https://pkgs.tailscale.com/stable/fedora/tailscale.repo -o /etc/yum.repos.d/tailscale.repo
 chmod 644 /etc/yum.repos.d/tailscale.repo
 dnf install -y tailscale
-
-# Install Asus stuff
-dnf5 -y copr enable lukenukem/asus-linux
-dnf5 -y install asusctl asusctl-rog-gui supergfxctl
-# Disable COPRs so they don't end up enabled on the final image:
-dnf5 -y copr disable lukenukem/asus-linux
